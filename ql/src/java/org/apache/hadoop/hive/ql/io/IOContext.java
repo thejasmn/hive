389736ee89f7383fc80cf1674b6befce72582215
Thejas Nair <thejas@hortonworks.com>
2013-10-27 23:41:31 -0700

    change IOContext inputfile to inputpath

--- ql/src/java/org/apache/hadoop/hive/ql/io/IOContext.java
+++ ql/src/java/org/apache/hadoop/hive/ql/io/IOContext.java
@@ -18,6 +18,8 @@
 
 package org.apache.hadoop.hive.ql.io;
 
+import org.apache.hadoop.fs.Path;
+
 
 /**
  * IOContext basically contains the position information of the current
@@ -67,7 +69,7 @@ public class IOContext {
     UNKNOWN
   }
 
-  String inputFile;
+  Path inputPath;
 
   public IOContext() {
     this.currentBlockStart = 0;
@@ -109,12 +111,12 @@ public class IOContext {
     this.isBlockPointer = isBlockPointer;
   }
 
-  public String getInputFile() {
-    return inputFile;
+  public Path getInputFile() {
+    return inputPath;
   }
 
-  public void setInputFile(String inputFile) {
-    this.inputFile = inputFile;
+  public void setInputFile(Path inputFile) {
+    this.inputPath = inputFile;
   }
 
   public void setIOExceptions(boolean ioe) {
