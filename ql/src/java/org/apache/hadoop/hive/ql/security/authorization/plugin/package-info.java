6fc31936126fd769114901720735e80822a13be1
Thejas Nair <thejas@hortonworks.com>
2014-02-07 11:27:18 -0800

    package level documentation

---
+++ ql/src/java/org/apache/hadoop/hive/ql/security/authorization/plugin/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * This package provides interfaces and classes that can be used to implement custom authorization for hive.
+ *
+ * How hive code uses this interface:
+ * The interface that hive code invokes is HiveAuthorizer class.
+ * The classes HivePrincipal, HivePrivilege, HivePrivilegeObject, HivePrivilegeInfo, HiveOperationType
+ * are arguments used in the authorization interface.
+ * The methods in the interface throws two types of exceptions - HiveAuthzPluginException (in
+ * case of internal errors), and HiveAuthzPluginDeniedException (when action is not permitted
+ * because authorization has failed).
+ *
+ * Hive uses the HiveAuthorizerFactory interface, whose implementing class is configurable through
+ * hive configuration, to instantiate an instance of this interface.
+ *
+ *
+ * Guide on implementing the interface:
+ * There are two categories of operations to be done by the authorization interface, one is the
+ * actions performed by the access control statements, which updates the privileges that have
+ * been granted (and stores in some where like metastore database), and also retrieves the current
+ * state of privileges. You may choose not to implement this part and juse a no-op implementation
+ * if you are going to manage the authorization externally (eg, if you base it on mapping to
+ *  file system permissions).
+ * The 2nd category of operation is authorizing the hive actions by checking against the privileges
+ * the user has on the objects.
+ * HiveAccessController has the interface for the first type of operations and
+ *  HiveAuthorizationValidator has interface for second type of operations.
+ *
+ * HiveAuthorizerImpl is a convenience class that you can use by just passing the implementations
+ * of these two interfaces (HiveAuthorizerImpl, HiveAuthorizationValidator) in the constructor.
+ *
+ */
+package org.apache.hadoop.hive.ql.security.authorization.plugin;
